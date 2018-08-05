7d82873ffb3cf84299f3a3744e87aad3a807830d
anykeyjunkie <github@j-riedel.de>
2018-08-05 19:15:32 +0200

    created new DirectoryPickerFragment to select directory for saving text documents. moved TextInputFragment in correct folder.

--- app/src/main/java/seemoo/fitbit/miscellaneous/ExternalStorage.java
+++ app/src/main/java/seemoo/fitbit/miscellaneous/ExternalStorage.java
@@ -2,6 +2,7 @@ package seemoo.fitbit.miscellaneous;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Environment;
 import android.util.Log;
 
@@ -24,7 +25,7 @@ public class ExternalStorage {
 
     private final static String TAG = ExternalStorage.class.getSimpleName();
 
-    public static String DIRECTORY = "Android/data/seemoo.fitbit/files/" + Environment.DIRECTORY_DOCUMENTS;
+    private static String directory = "";
 
     /**
      * Sets the directory for loading/saving files.
@@ -32,11 +33,23 @@ public class ExternalStorage {
      */
     public static void setDirectory(String value, Activity activity){
         if(value == null || value.equals("")){
-            DIRECTORY = "Android/data/seemoo.fitbit/files/" + Environment.DIRECTORY_DOCUMENTS;
+            directory = getDefaultPath(activity);
         } else {
-            DIRECTORY = value;
+            directory = value;
         }
-        InternalStorage.saveString(DIRECTORY, ConstantValues.SETTING_DIRECTORY, activity);
+        InternalStorage.saveString(directory, ConstantValues.SETTING_DIRECTORY, activity);
+    }
+
+    private static String getDefaultPath(Context context) {
+        return context.getExternalFilesDir(null).getPath();
+    }
+
+    public static String getDirectory(Context context){
+        if(directory != ""){
+            return directory;
+        } else {
+            return getDefaultPath(context);
+        }
     }
 
     /**
@@ -65,13 +78,16 @@ public class ExternalStorage {
      */
     public static void saveString(String string, String name, Activity activity){
         if(isExternalStorageWritable()){
-            File path = activity.getExternalFilesDir("../../../../" + DIRECTORY);
-            File file = new File(path, name + "_" + FitbitDevice.getMacAddress());
+            if(directory == ""){
+                directory = getDefaultPath(activity);
+            }
+            File file = new File(directory, name + "_" + FitbitDevice.getMacAddress());
             try {
                 FileOutputStream outputStream = new FileOutputStream(file);
                 outputStream.write(string.getBytes());
                 outputStream.close();
-                Log.e(TAG, "saved file on external storage: " + name + "_" + FitbitDevice.getMacAddress());
+                Log.e(TAG, "saved file on external storage: " + directory + "\\" +
+                        name + "_" + FitbitDevice.getMacAddress());
             } catch(IOException e){
                 Log.e(TAG, e.toString());
             }
@@ -100,7 +116,7 @@ public class ExternalStorage {
     public static String loadString(String name, Activity activity){
         String result = "";
         if(isExternalStorageReadable()){
-            File path = activity.getExternalFilesDir("../../../../" + DIRECTORY);
+            File path = activity.getExternalFilesDir("../../../../" + directory);
             File file = new File(path, name);
             try {
                 FileInputStream inputStream = new FileInputStream(file);
