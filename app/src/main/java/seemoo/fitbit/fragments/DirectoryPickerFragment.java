8c4e29369ee5359bd4a734fab2b4dc59f46581b3
anykeyjunkie <github@j-riedel.de>
2018-08-23 13:58:12 +0200

    merged code to get path from android file/directory picker. solves problem with "downloads" selection in fw flash dialog

--- app/src/main/java/seemoo/fitbit/fragments/DirectoryPickerFragment.java
+++ app/src/main/java/seemoo/fitbit/fragments/DirectoryPickerFragment.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 
 import seemoo.fitbit.R;
 import seemoo.fitbit.activities.WorkActivity;
+import seemoo.fitbit.miscellaneous.FileUriHelper;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -89,31 +90,13 @@ public class DirectoryPickerFragment extends Fragment {
             Uri uri = data.getData();
             Uri docUri = DocumentsContract.buildDocumentUriUsingTree(uri,
                     DocumentsContract.getTreeDocumentId(uri));
-            String path = DirectoryPickerFragment.getPath(getContext(), docUri);
+            String path = FileUriHelper.getPath(getContext(), docUri);
             onDirectoyDialogResult(path);
         }
     }
 
     public void onDirectoyDialogResult(String path){
         editText.setText(path);
-    }
-
-    public static String getPath(final Context context, final Uri uri) {
-        if (DocumentsContract.isDocumentUri(context, uri) &&
-                "com.android.externalstorage.documents".equals(uri.getAuthority())) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-                if ("primary".equalsIgnoreCase(type)) {
-                    if(split.length > 1){
-                        return Environment.getExternalStorageDirectory() + "/" + split[1];
-                    } else {
-                        return Environment.getExternalStorageDirectory().getPath();
-                    }
-                }
-        }
-        Toast.makeText(context, "Error. Only internal storage supported.",Toast.LENGTH_LONG).show();
-        return "";
     }
 
 }
